766fccbf2283025a57b73c1bca2456bbcc5b51c1
susheel <susheel455@gmail.com>
2016-07-11 13:09:26 +0530

    Initial Commit

---
+++ LiveTweets/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  LiveTweets
+//
+//  Created by Ajeet N on 11/07/16.
+//  Copyright © 2016 AhexTech. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
